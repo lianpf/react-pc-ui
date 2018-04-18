3127441343b726e7115ddc4fd786aa930bc55fdf
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-18 14:26:01 +0800

    add@ example 入口初始文件

---
+++ examples/router.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { Router, Route, hashHistory } from 'react-router';
+// import HomePage from './pages/homePage'
+// import Button from './pages/button'
+// import Loading from './pages/loading'
+// import Video from './pages/video'
+// import Picker from './pages/picker'
+// import Popup from './pages/popup'
+// import BaseList from './pages/baseList'
+// import Swiper from './pages/swiper'
+// import Toast from './pages/toast'
+// import Tab from './pages/tab'
+// import Slider from './pages/slider'
+// function RouterConfig({ history }) {
+//   return (
+//     <Router history={hashHistory}>
+//       <Route path="/" component={HomePage}/>
+//       <Route path="/Button" component={Button}/>
+//       <Route path="/Loading" component={Loading}/>
+//       <Route path="/Video" component={Video}/>
+//       <Route path="/Picker" component={Picker}/>
+//       <Route path="/Popup" component={Popup}/>
+//       <Route path="/BaseList" component={BaseList}/>
+//       <Route path="/Swiper" component={Swiper}/>
+//       <Route path="/Toast" component={Toast}/>
+//       <Route path="/Tab" component={Tab}/>
+//       <Route path="/Slider" component={Slider}/>
+//     </Router>
+//   )
+// }
+// export default RouterConfig;
