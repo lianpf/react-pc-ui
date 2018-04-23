168f63d07c434689a327a23f63ee6e0799e7b7fe
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-23 19:14:34 +0800

    add@ button & table 组件初始化

---
+++ src/component/button/index.js
@@ -0,0 +1,21 @@
+import React, { PropTypes } from 'react';
+import {connect} from 'react-redux';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>--组件库-lian-button-组件--</div>
+    );
+  }
+}
+// function mapStateToProps(state) {
+//   const { borrowMoney } = state;
+//   return { borrowMoney };
+// }
+
+export default connect()(Index);
+
