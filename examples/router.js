16040ca5dc5ff140fd434979c96b2d3fe62e5d59
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-23 11:34:41 +0800

    add@ router配置文件

--- examples/router.js
+++ examples/router.js
@@ -1,31 +1,27 @@
-// import React from 'react';
-// import { Router, Route, hashHistory } from 'react-router';
-// import HomePage from './pages/homePage'
-// import Button from './pages/button'
-// import Loading from './pages/loading'
-// import Video from './pages/video'
-// import Picker from './pages/picker'
-// import Popup from './pages/popup'
-// import BaseList from './pages/baseList'
-// import Swiper from './pages/swiper'
-// import Toast from './pages/toast'
-// import Tab from './pages/tab'
-// import Slider from './pages/slider'
-// function RouterConfig({ history }) {
-//   return (
-//     <Router history={hashHistory}>
-//       <Route path="/" component={HomePage}/>
-//       <Route path="/Button" component={Button}/>
-//       <Route path="/Loading" component={Loading}/>
-//       <Route path="/Video" component={Video}/>
-//       <Route path="/Picker" component={Picker}/>
-//       <Route path="/Popup" component={Popup}/>
-//       <Route path="/BaseList" component={BaseList}/>
-//       <Route path="/Swiper" component={Swiper}/>
-//       <Route path="/Toast" component={Toast}/>
-//       <Route path="/Tab" component={Tab}/>
-//       <Route path="/Slider" component={Slider}/>
-//     </Router>
-//   )
-// }
-// export default RouterConfig;
+import React from 'react';
+import { Router, Route, Switch } from 'react-router';
+
+
+// 组件 button
+const Button = (location, cb) => {
+  require.ensure([], (require) => {
+    cb(null, require('./button/index').default)
+  }, 'button');
+};
+
+// 组件 table
+const Table = (location, cb) => {
+  require.ensure([], (require) => {
+    cb(null, require('./table/index').default)
+  }, 'table')
+};
+
+
+export default ({ history }) => (
+    <Router history={history}>
+      <Switch>
+        <Route path="/button" component={Button}/>
+        <Route path="/table" component={Table}/>
+      </Switch>
+    </Router>
+)
