57482e5f830549332c5a8a8fcabb1e9f6305b9dc
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-23 15:29:11 +0800

    update@ 更改路由为同步router

--- examples/router.js
+++ examples/router.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 // 组件 button
-const Button = (location, cb) => {
-  require.ensure([], (require) => {
-    cb(null, require('./button/index').default)
-  }, 'button');
-};
+// const Button = (location, cb) => {
+//   require.ensure([], (require) => {
+//     cb(null, require('./button/index').default)
+//   }, 'button');
+// };
 
 // 组件 table
-const Table = (location, cb) => {
-  require.ensure([], (require) => {
-    cb(null, require('./table/index').default)
-  }, 'table')
-};
+// const Table = (location, cb) => {
+//   require.ensure([], (require) => {
+//     cb(null, require('./table/index').default)
+//   }, 'table')
+// };
+
+import Button from './button/index';
+import Table from './table/index';
 
 
-export default ({ history }) => (
-    <Router history={history}>
+export default () => (
+    <Router>
       <Switch>
         <Route path="/button" component={Button}/>
         <Route path="/table" component={Table}/>
