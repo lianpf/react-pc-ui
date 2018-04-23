cd7b4496f575373039f1aae5dabf89d08ead6a60
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-23 19:10:16 +0800

    update@ webpack.dev.config.js 配置文件

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -5,10 +5,10 @@
 var path = require('path');
 var webpack = require('webpack');
 // var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
 var webpackBaseConfig = require('./webpack.base.config.js');
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = merge(webpackBaseConfig, {
   /**
    * 入口
@@ -30,6 +30,14 @@ module.exports = merge(webpackBaseConfig, {
     chunkFilename: '[name].chunk.js'
   },
   /**
+   *
+   * */
+  // devtool: 'inline-source-map',
+  // devServer: {
+  //   // contentBase: './dist',
+  //   hot: true
+  // },
+  /**
    * mode
    * 环境
    * */
@@ -39,12 +47,16 @@ module.exports = merge(webpackBaseConfig, {
    * 插件的范围包括，从打包优化和压缩，一直到重新定义环境中的变量, 可以用来处理各种各样的任务
    * */
   plugins: [
-    // new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: 'vendor.bundle.js' }),
+    new FriendlyErrorsPlugin(),
+    // new webpack.HotModuleReplacementPlugin()
+    /** HtmlWebpackPlugin
+     * HtmlWebpackPlugin 创建了一个全新的index.html 文件去替换原来的index.html，所有的 bundle 会自动添加到新的 html 中
+     * */
     new HtmlWebpackPlugin({
+      // title: 'component library'
       inject: true,
       filename: path.join(__dirname, '../examples/dist/index.html'),
       template: path.join(__dirname, '../examples/index.html')
     }),
-    new FriendlyErrorsPlugin()
   ]
 });
