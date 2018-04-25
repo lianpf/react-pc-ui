d3c57ba2aa1373187c175874b25cbb2c523280a7
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-25 16:59:05 +0800

    update@ 修改library暴露公共index.js文件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router';
-import RouterConfig from './router.js';
-import 'babel-polyfill';
-import './styles/index.less'
-ReactDOM.render(
-  <RouterConfig/>
-, document.getElementById('app'));
+export { default as Button } from './component/button';
+export { default as Table } from './component/table';
