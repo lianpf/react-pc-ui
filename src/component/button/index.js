b0ce60526383d81394ab4efa6725901c86fbac03
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-25 16:57:52 +0800

    update@ button 公共组件

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,14 +1,38 @@
 import React, { PropTypes } from 'react';
 import {connect} from 'react-redux';
+import './index.less';
 
 class Index extends React.Component {
+  // static propTypes = {
+  //   bgColor : React.PropTypes.string,
+  //   color : React.PropTypes.string,
+  //   type : React.PropTypes.string,
+  //   onClick: React.PropTypes.func,
+  //   loading: React.PropTypes.bool,
+  //   loadingColor: React.PropTypes.string,
+  //   loadingSize: React.PropTypes.number,
+  //   disabled: React.PropTypes.bool,
+  //   radius: React.PropTypes.number,
+  //   fontSize: React.PropTypes.number
+  // }
+  // static defaultProps = {
+  //   bgColor: '#DA3D42',
+  //   color: '#fff',
+  //   type : 'default',
+  //   loading: false,
+  //   loadingColor: '#FFFF00',
+  //   loadingSize: 14,
+  //   disabled: false,
+  //   radius: 20,
+  //   fontSize: 16
+  // }
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-        <div>--组件库-lian-button-组件--</div>
+        <button className={`react-pc-ui-button`}>{this.props.children}</button>
     );
   }
 }
