66b1c2fa309ccd2e1f2cb36187c895e3972d6639
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-27 19:46:31 +0800

    add@ example button

--- examples/button/index.js
+++ examples/button/index.js
@@ -1,8 +1,21 @@
 import React, { PropTypes } from 'react';
 import {connect} from 'react-redux';
 
-import Button from '../../lib/button/index';
+// import Button from '../../lib/button/index';
+import Button from '../../src/component/button/index';
 import './index.less';
+
+const params = {
+  bgColor: '#DA3D42',
+  color: '#fff',
+  type : 'default',
+  // loading: false,
+  // loadingColor: '#FFFF00',
+  loadingSize: 14,
+  disabled: false,
+  radius: 20,
+  fontSize: 16
+};
 
 class Index extends React.Component {
   constructor(props) {
@@ -16,7 +29,7 @@ class Index extends React.Component {
        <div>
          <p>--展示-button-组件-0-</p>
          <div className={`layout`}>hah</div>
-         <Button>1</Button>
+         <Button {...params}>Default</Button>
        </div>
     );
   }
