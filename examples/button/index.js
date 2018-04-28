9728acdbef981e2c3f96ab0808ae63064fb33320
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-28 18:07:44 +0800

    update@ 更改example button 组件

--- examples/button/index.js
+++ examples/button/index.js
@@ -1,9 +1,11 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 
-// import Button from '../../lib/button/index';
-import Button from '../../src/component/button/index';
+import Button from '../../lib/button/index';
+// import Button from '../../src/component/button/index';
 import './index.less';
+
+import Template from '../template/index';
 
 const params = {
   bgColor: '#DA3D42',
@@ -17,17 +19,91 @@ const params = {
   fontSize: 16
 };
 
+const options = {
+  title: 'Button',
+  desc: '按钮组件',
+  params:[{
+    name: 'type',
+    des: '按钮大小',
+    type: 'String',
+    values:[
+      'default（默认）',
+      'large',
+      'small'
+    ],
+    default: 'default'
+  }, {
+    name: 'bgColor',
+    des: '按钮背景色',
+    type: 'String',
+    values:[
+      '16进制',
+      'RGB',
+      '颜色英文名称'
+    ],
+    default: '#DA3D42'
+  }, {
+    name: 'color',
+    des: '按钮文本景色',
+    type: 'String',
+    values:[
+      '16进制',
+      'RGB',
+      '颜色英文名称'
+    ],
+    default: '#fff'
+  }, {
+    name: 'disabled',
+    des: '按钮禁用',
+    type: 'Boolean',
+    values:[
+      'true',
+      'false'
+    ],
+    default: 'false'
+  }, {
+    name: 'radius',
+    des: '按钮圆角大小',
+    type: 'Number',
+    values:[
+      'number数字'
+    ],
+    default: '20'
+  }, {
+    name: 'fontSize',
+    des: '设置按钮字体大小',
+    type: 'Number',
+    values:[
+      'number数字'
+    ],
+    default: '16'
+  }]
+};
+
 class Index extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const Style = {"background": "green"};
     return (
        <div className={`button-layout`}>
-         <div>--展示-button-组件-0-</div>
-         <Button {...params}>Default</Button>
+         <h1>{options.title}</h1>
+         <p>{options.desc}</p>
+         <h2>具体语法</h2>
+         <div className={'component-demo-list'}>
+           {
+             options.params.length && options.params.map((item, index) => {
+               return (
+                   <div key={index} className={'component-demo-item'}>
+                     --展示-button-组件-0-
+                     <Button {...params}>Default</Button>
+                   </div>
+               );
+             })
+           }
+         </div>
+         <Template {...options} />
        </div>
     );
   }
