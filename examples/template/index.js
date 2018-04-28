641f639f3797ee016fe50642dd6fa15c5d2c0d96
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-04-28 18:07:04 +0800

    add@ 新增template 组件

---
+++ examples/template/index.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+
+import './index.less';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { title, desc, params} = this.props;
+    return (
+       <div className={`button-layout`}>
+         {this.props.children}
+       </div>
+    );
+  }
+}
+
+export default Index;
+
