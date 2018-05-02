d92b7b40494dc2327d535eb3657ad6d3fe7c0c9f
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-05-02 17:11:47 +0800

    update@ 修改example -> table demo

--- examples/table/index.js
+++ examples/table/index.js
@@ -8,32 +8,37 @@ import Table from '../../src/component/table/index';
 
 const dataSource = [{
   key: '1',
-  name: '胡彦斌',
+  name: 'John Brown',
   age: 32,
-  address: '西湖区湖底公园1号'
+  address: 'New York No. 1 Lake Park',
 }, {
   key: '2',
-  name: '胡彦祖',
+  name: 'Jim Green',
   age: 42,
-  address: '西湖区湖底公园1号'
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
 }];
 
 const columns = [{
-  title: '姓名',
+  title: 'Name',
   dataIndex: 'name',
   key: 'name',
+  render: text => <a href="javascript:;">{text}</a>,
 }, {
-  title: '年龄',
+  title: 'Age',
   dataIndex: 'age',
   key: 'age',
 }, {
-  title: '住址',
+  title: 'Address',
   dataIndex: 'address',
   key: 'address',
 }, {
-  title: '操作',
-  dataIndex: '',
-  key: '',
+  title: 'Action',
+  key: 'action',
   render: (text, record) => (
       <span>
       <a href="javascript:;">Action 一 {record.name}</a>
@@ -131,7 +136,47 @@ const options = {
       }
     ],
     default: '16'
-  }]
+  }],
+  api: [
+    {
+      name: 'table',
+      dataSource: [{
+        param: 'columns',
+        desc: '表格列的配置描述',
+        type: 'json数组(Object Array)',
+        default: '-',
+      }, {
+        param: 'dataSource',
+        desc: '数据数组',
+        type: 'json数组(Object Array)',
+        default: '-',
+      }]
+    },
+    {
+      name: 'Column',
+      dataSource: [{
+        param: 'dataIndex',
+        desc: '列数据在数据项(dataSource)中对应的key',
+        type: 'string',
+        default: '-',
+      }, {
+        param: 'key',
+        desc: 'React 需要的 key，如果已经设置了唯一的 dataIndex，可以忽略这个属性',
+        type: 'string',
+        default: '-',
+      }, {
+        param: 'title',
+        desc: '列头显示文字',
+        type: 'string|ReactNode',
+        default: '-',
+      }, {
+        param: 'render',
+        desc: '生成复杂数据的渲染函数, 参数分别为当前行的值, 当前行数据',
+        type: 'Function(text, record) {}',
+        default: '-',
+      }]
+    }
+  ]
 };
 
 class Index extends React.Component {
