d35dace35d1f810f661747ae1c0d6bb2977d2f2e
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-05-02 17:12:33 +0800

    update@ 优化 template 展示 API

--- examples/template/index.js
+++ examples/template/index.js
@@ -2,7 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 
 import './index.less';
+import Table from '../../src/component/table';
 
+const columns = [{
+  title: '参数',
+  dataIndex: 'param',
+  key: 'param',
+}, {
+  title: '说明',
+  dataIndex: 'desc',
+  key: 'desc',
+}, {
+  title: '类型',
+  dataIndex: 'type',
+  key: 'type',
+}, {
+  title: '默认值',
+  dataIndex: 'default',
+  key: 'default',
+}];
 
 class Index extends React.Component {
   constructor(props) {
@@ -19,6 +37,19 @@ class Index extends React.Component {
           <p>{options.func}</p>
           <h2>效果展示</h2>
           {this.props.children}
+
+          <h2>API</h2>
+          {
+            options.api.length && options.api.map((item, index) => {
+              return (
+                  <div key={`${item.name}-api-${index}`}>
+                    <h4>{item.name}</h4>
+                    <Table dataSource={item.dataSource} columns={columns} />
+                  </div>
+              );
+            })
+          }
+          <Table />
         </div>
     );
   }
