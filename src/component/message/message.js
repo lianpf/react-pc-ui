764b95a1164e61a8fa185e3560fd0937d7be2b58
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-05-17 14:55:54 +0800

    add@ message 差异化处理 & onClose func处理

--- src/component/message/message.js
+++ src/component/message/message.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import SuccessIcon from './img/check.png';
+import InfoIcon from './img/infomation.png';
+import ErrorIcon from './img/close.png';
+
 export default class Message extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +12,7 @@ export default class Message extends Component {
     };
     this.dismiss = this.dismiss.bind(this);
     this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.getMsgType = this.getMsgType.bind(this);
   }
 
   componentDidMount() {
@@ -18,7 +23,11 @@ export default class Message extends Component {
   }
 
   dismiss() {
-    this.props.onClose(this.props.id);
+    const {id, close, onClose} = this.props;
+    close(id);
+    if (onClose && typeof onClose === 'function') {
+      onClose();
+    }
   }
 
   handleMouseOver() {
@@ -26,16 +35,40 @@ export default class Message extends Component {
     this.setState({ duration: 0 });
   }
 
+  getMsgType(type) {
+    const styleArr = {
+      success: {
+        style: "react-pc-ui-message-success",
+        icon: SuccessIcon,
+      },
+      info: {
+        style: "react-pc-ui-message-info",
+        icon: InfoIcon,
+      },
+      error: {
+        style: "react-pc-ui-message-error",
+        icon: ErrorIcon,
+      }
+    };
+    return (
+        <div className={`react-pc-ui-message-icon ${styleArr[type].style}`}>
+          <img src={styleArr[type].icon} alt=""/>
+        </div>
+    );
+  }
+
   render() {
     const { content, ...props } = this.props;
     // const { duration } = this.state;
+    console.log('--props--', props);
     delete props.duration;
 
     return (
-        <div
-            className="alert alert-info alert-msg"
-        >
-          {content}
+        <div className="react-pc-ui-message-common">
+          <div className="react-pc-ui-message-content">
+            {this.getMsgType(props.type)}
+            {content}
+          </div>
         </div>
     );
   }
