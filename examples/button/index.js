9856738f82c5315a2be66d69a84b3d002215545b
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-05-17 16:41:36 +0800

    update@ message config文件

--- examples/button/index.js
+++ examples/button/index.js
@@ -1,8 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 
-// import Button from '../../lib/button/index';
-import Button from '../../src/component/button/index';
+import Button from '../../lib/button/index';
 import '../template/index.less';
 
 import Template from '../template/index';
--- examples/table/index.js
+++ examples/table/index.js
@@ -3,8 +3,7 @@ import {connect} from 'react-redux';
 
 import '../template/index.less';
 import Template from '../template/index';
-import DemoPanel from '../template/demo-panel';
-import Table from '../../src/component/table/index';
+import Table from '../../lib/table';
 
 const dataSource = [{
   key: '1',
@@ -50,93 +49,7 @@ const options = {
   title: 'Table',
   desc: '表格组件',
   func: 'onClick',
-  params:[{
-    name: 'type',
-    des: '按钮大小',
-    type: 'String',
-    values:[{
-      name: 'default',
-      value: 'default',
-    }, {
-      name: 'large',
-      value: 'large',
-    }, {
-      name: 'small',
-      value: 'small',
-    }],
-    default: 'default'
-  }, {
-    name: 'bgColor',
-    desc: '按钮背景色',
-    type: 'String',
-    values:[
-      {
-        name: '16进制',
-        value: '#1E90FF',
-      }, {
-        name: 'RGB',
-        value: 'rgb(123,104,238)',
-      }, {
-        name: '颜色英文名称',
-        value: 'LightCoral',
-      }
-    ],
-    default: '#DA3D42'
-  }, {
-    name: 'color',
-    desc: '按钮文本景色',
-    type: 'String',
-    values:[
-      {
-        name: '16进制',
-        value: '#fff',
-      }, {
-        name: 'RGB',
-        value: 'rgb(0,0,0)',
-      }, {
-        name: '颜色英文名称',
-        value: 'black',
-      }
-    ],
-    default: '#fff'
-  }, {
-    name: 'disabled',
-    desc: '按钮禁用',
-    type: 'Boolean',
-    values:[
-      {
-        name: '是',
-        value: true,
-      },
-      {
-        name: '否',
-        value: false,
-      }
-    ],
-    default: 'false'
-  }, {
-    name: 'radius',
-    desc: '按钮圆角大小',
-    type: 'Number',
-    values:[
-      {
-        name: 'number数字',
-        value: '5',
-      }
-    ],
-    default: '20'
-  }, {
-    name: 'fontSize',
-    desc: '设置按钮字体大小',
-    type: 'Number',
-    values:[
-      {
-        name: 'number数字',
-        value: '18',
-      }
-    ],
-    default: '16'
-  }],
+  params:[],
   api: [
     {
       name: 'table',
