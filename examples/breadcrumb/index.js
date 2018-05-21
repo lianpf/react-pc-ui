22f3714d435449b8e28d103277cfb9b77a6a25ab
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-05-21 19:56:58 +0800

    add@ breadcrumb example 组件

---
+++ examples/breadcrumb/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import '../template/index.less';
+import Template from '../template/index';
+import Button from '../../lib/button';
+import Modal from '../../lib/modal';
+
+import Breadcrumb from '../../src/component/breadcrumb/index';
+
+const options = {
+  title: 'Breadcrumb',
+  desc: '显示当前页面在系统层级结构中的位置，并能向上返回',
+  func: '-',
+  params:[],
+  api: [
+    {
+      name: 'Breadcrumb 配置参数',
+      dataSource: [{
+        param: 'paths',
+        desc: '配置参数',
+        type: 'object[]',
+        default: '-',
+      }]
+    },
+    {
+      name: 'paths Item 配置参数',
+      dataSource: [{
+        param: 'url',
+        desc: '跳转链接',
+        type: 'string',
+        default: '-',
+      }, {
+        param: 'text',
+        desc: '展示文案',
+        type: 'string',
+        default: '-',
+      }]
+    }
+  ]
+};
+
+const paths = [
+  {
+    'path': '/#/component',
+    'text': '首页'
+  },
+  {
+    'path': '',
+    'text': '一级面包屑'
+  },
+  {
+    'path': '',
+    'text': '当前页面'
+  }
+];
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+        <div>
+          <Template options={options}>
+            <div style={{background: "rgba(0, 0, 0, .1)", padding: '8px', borderRadius: '5px'}}>
+              <Breadcrumb paths={paths} />
+            </div>
+          </Template>
+        </div>
+    );
+  }
+}
+
+export default connect()(Index);
+
