923b10e4e3deea7eb3fe3f70a5e7fb4e836d35df
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-06-05 10:09:00 +0800

    update@ slider 增加 disabled & value & defaultValue属性

--- examples/slider/index.js
+++ examples/slider/index.js
@@ -44,7 +44,8 @@ const options = {
         default: '1',
       }, {
         param: 'value',
-        desc: '设置当前取值',
+        desc: '设置当前取值, 注意: 此处输入框一定要为合理value, 比如, 假如此处min为20, max为50, step为6, 那么第一, value >=20 && value <=50, ' +
+        '第二, 可输入值为 min + step * 整数, 即20, 26, 32, 38, 44, 50。这些条件需要调用者自行控制',
         type: 'number',
         default: '-',
       }, {
@@ -66,16 +67,17 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 35
+      value: 25
     }
   }
 
-  tabsChange(key) {
-    console.log('--tabs-change--', key);
-  }
   sliderChange(value) {
     console.log('--example--', value);
     this.setState({value})
+  }
+  changeField(e) {
+    console.log('--手动修改--', e.target.value);
+    this.setState({value: Number(e.target.value)})
   }
 
   render() {
@@ -85,9 +87,18 @@ class Index extends React.Component {
             <div
                 style={{border: "1px solid rgba(0, 0, 0, .1)",
                   padding: '8px', borderRadius: '5px', width: '500px', display: 'inline-block'}}>
-              <Slider onChange={(value) => this.sliderChange(value)}/>
+              <Slider onChange={(value) => this.sliderChange(value)} value={this.state.value}/>
             </div>
-            <div style={{display: 'inline-block', verticalAlign: 'top', marginLeft: "10px"}}>value: {this.state.value}</div>
+            <div style={{display: 'inline-block', verticalAlign: 'top', marginLeft: "10px"}}>
+              value: {this.state.value}
+              &nbsp;|&nbsp;
+              手动输入: <input type="text" value={this.state.value}
+                           style={{width:"120px", height: "21px",
+                             borderBottomLeftRadius: "3px", borderBottomRightRadius: "3px",
+                             borderTopLeftRadius: "3px", borderTopRightRadius: "3px",
+                             outline: 'none', border: "1px solid rgba(0, 0, 0, .65)"}}
+                           placeholder="0~100之间" onChange={(e) => this.changeField(e)}/>
+              </div>
           </Template>
         </div>
     );
--- src/component/slider/slider.js
+++ src/component/slider/slider.js
@@ -8,11 +8,10 @@ import './index.less';
 
 const prefixCls = 'react-pc-ui-slider';
 
-
 export default class Index extends React.Component{
   static defaultProps = {
     vertical: false,
-    disabled: true,
+    disabled: false,
     max: 100,
     min: 0,
     step: 1,
@@ -29,6 +28,11 @@ export default class Index extends React.Component{
   constructor(props) {
     super(props);
 
+    const defaultValue = props.defaultValue !== undefined ?
+        props.defaultValue : props.min;
+    const value = props.value !== undefined ?
+        props.value : defaultValue;
+
     this.state = {
       parentBasicX: 0,
       parentWidthCount: 0,
@@ -36,7 +40,11 @@ export default class Index extends React.Component{
       currentState: 0,
       previewValue: props.min,
       currentValue: props.min,
+      defaultValue,
+      value,
     };
+    this.countPosition = this.countPosition.bind(this);
+    this.isLegal = this.isLegal.bind(this);
   }
 
   componentDidMount() {
@@ -44,6 +52,7 @@ export default class Index extends React.Component{
     let t = 0;
     let l = 0;
     let parentWidthCount = event.offsetWidth;
+    const self = this;
 
     while (event = event.offsetParent) {
       t += event.offsetTop;
@@ -52,6 +61,31 @@ export default class Index extends React.Component{
     this.setState({
       parentBasicX: l,
       parentWidthCount,
+    });
+
+    const _bool = this.isLegal(self.state.value);
+    if (!_bool) {
+      return;
+    }
+    this.countPosition(self.state.value);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!('value' in nextProps || 'min' in nextProps || 'max' in nextProps)) return;
+
+    const prevValue = this.state.value;
+    const value = nextProps.value !== undefined ?
+        nextProps.value : prevValue;
+    if (value === prevValue) return;
+
+    const _bool = this.isLegal(value);
+    if (!_bool) {
+      return;
+    }
+    this.setState({
+      value: value
+    }, () => {
+      this.countPosition();
     });
   }
 
@@ -71,12 +105,35 @@ export default class Index extends React.Component{
     let percentWidth =  roundTimes * step / (max -min) * 100;
     children[0].style.width = `${percentWidth}%`;
     children[1].style.left = `${percentWidth}%`;
-    console.log("currentValue--", currentValue);
+
     this.setState({
-      currentValue,
+      value: currentValue,
     }, () => {
-      this.props.onChange(this.state.currentValue);
+      this.props.onChange(this.state.value);
     })
+  }
+
+  // 判断value是否合理
+  isLegal(value) {
+    // 不合理的值
+    let {max, min, step} = this.props;
+    const _bool = value <= max && value >= min && ((value - min) % step === 0);
+    if (!_bool) {
+      return false;
+    }
+    return true;
+  }
+
+  // 根据传入value 计算position
+  countPosition(value) {
+    let {max, min, step} = this.props;
+    let percentWidth = (value - min) / (max - min) * 100;
+    let sliderH = this.refs['sliderH'];
+    let children = sliderH.getElementsByTagName('div');
+    children[0].style.width = `${percentWidth}%`;
+    children[1].style.left = `${percentWidth}%`;
+
+    this.props.onChange(value);
   }
 
   render(){
