e13a86f90beccec3f15a3238d315e33322428ef7
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-06-13 14:41:01 +0800

    update@ pagination component 页数省略配置

--- src/component/pagination/pagination.js
+++ src/component/pagination/pagination.js
@@ -30,7 +30,7 @@ export default class Pagination extends React.Component{
     defaultCurrent: 1,
     // pageSize: void 0,
     defaultPageSize: 10,
-    total: 80,
+    total: 100,
     size: "",
     showQuickJumper: false,
     hideOnSinglePage: false,
@@ -63,6 +63,8 @@ export default class Pagination extends React.Component{
       current,
       pageSize,
       goInputText: '',
+      pageBufferSize: 2, // totalPage 大于 5 + pageBufferSize*2 时, current 前后展示 page 个数
+      jumpSize: 5,
     };
 
     this.changePage = this.changePage.bind(this);
@@ -81,6 +83,7 @@ export default class Pagination extends React.Component{
       });
     }
   }
+  // update current
   changePage(page) {
     this.setState({
       current: page,
@@ -99,7 +102,7 @@ export default class Pagination extends React.Component{
       this.changePage(this.state.current + 1);
     }
   };
-  // 计算 totalPage
+  // calculate totalPage
   calculateTotalPage = () => {
     const { total } = this.props;
     const { pageSize } = this.state;
@@ -142,24 +145,124 @@ export default class Pagination extends React.Component{
     }
   };
 
+  getJumpPrevPage() {
+    return Math.max(1, this.state.current - this.state.jumpSize);
+  }
+
+  getJumpNextPage() {
+    return Math.min(this.calculateTotalPage(), this.state.current + this.state.jumpSize);
+  }
+
+  jumpPrev = () => {
+    this.changePage(this.getJumpPrevPage());
+  }
+
+  jumpNext = () => {
+    this.changePage(this.getJumpNextPage());
+  }
+
 
 
   render(){
     const { total } = this.props;
-    const { pageSize, current } = this.state;
-
+    const { pageSize, current, pageBufferSize } = this.state;
     const totalPage = this.calculateTotalPage();
 
+    let jumpPrev = null;
+    let jumpNext = null;
+    let lastPager = null;
+    let firstPager = null;
     let pageList = [];
-    for(let i = 0; i < totalPage; i++) {
-      pageList.push(<li
-          className={`${prefixCls}-pageList-basic
+    if ( totalPage <= 5 + pageBufferSize * 2) {
+      for(let i = 0; i < totalPage; i++) {
+        pageList.push(<li
+            className={`${prefixCls}-pageList-basic
           ${prefixCls}-pageList-large
           ${current === i + 1 ? `${prefixCls}-pageList-large-active` : ''}`}
-          key={`page-${i}`}
-          onClick={() => this.changePage(i+1)}
-      >{i + 1}</li>)
+            key={`page-${i}`}
+            onClick={() => this.changePage(i+1)}
+        >{i + 1}</li>)
+      }
+    } else {
+      jumpPrev = (
+          <li
+              key="prev"
+              onClick={this.jumpPrev}
+              tabIndex="0"
+              // onKeyPress={this.runIfEnterJumpPrev}
+              className={`${prefixCls}-jump-prev`}
+          >...</li>
+      );
+      jumpNext = (
+          <li
+              key="next"
+              tabIndex="0"
+              onClick={this.jumpNext}
+              // onKeyPress={this.runIfEnterJumpNext}
+              className={`${prefixCls}-jump-next`}
+          >...</li>
+      );
+      lastPager = (
+          <li
+              className={`${prefixCls}-pageList-basic
+              ${prefixCls}-pageList-large
+              ${current === totalPage ? `${prefixCls}-pageList-large-active` : ''}`}
+              key={`page-${totalPage}`}
+              onClick={() => this.changePage(totalPage)}
+          >{totalPage}</li>
+      );
+      firstPager = (
+          <li
+              className={`${prefixCls}-pageList-basic
+              ${prefixCls}-pageList-large
+              ${current === 1 ? `${prefixCls}-pageList-large-active` : ''}`}
+              key={`page-${1}`}
+              onClick={() => this.changePage(1)}
+          >{1}</li>
+      );
+
+      let left = Math.max(1, current - pageBufferSize);
+      let right = Math.min(current + pageBufferSize, totalPage);
+      if (current - 1 <= pageBufferSize) {
+        right = 1 + pageBufferSize * 2;
+      }
+      if (totalPage - current <= pageBufferSize) {
+        left = totalPage - pageBufferSize * 2;
+      }
+
+      for(let i = left; i <= right; i++) {
+        pageList.push(<li
+            className={`${prefixCls}-pageList-basic
+          ${prefixCls}-pageList-large
+          ${current === i ? `${prefixCls}-pageList-large-active` : ''}`}
+            key={`page-${i}`}
+            onClick={() => this.changePage(i)}
+        >{i}</li>)
+      }
+
+      if (current - 1 >= pageBufferSize * 2 && current !== 1 + 2) {
+        // pageList[0] = React.cloneElement(pagerList[0], {
+        //   className: `${prefixCls}-item-after-jump-prev`,
+        // });
+        pageList[0] = jumpPrev;
+        // pageList.unshift(jumpPrev);
+      }
+      if (totalPage - current >= pageBufferSize * 2 && current !== totalPage - 2) {
+        // pageList[pageList.length - 1] = React.cloneElement(pageList[pageList.length - 1], {
+        //   className: `${prefixCls}-item-before-jump-next`,
+        // });
+        pageList[pageList.length - 1] = jumpNext;
+        // pageList.push(jumpNext);
+      }
+
+      if (left !== 1) {
+        pageList.unshift(firstPager);
+      }
+      if (right !== totalPage) {
+        pageList.push(lastPager);
+      }
     }
+
     return (
         <div className={`${prefixCls}`}>
           <ul className={`${prefixCls}-pageList`}>
