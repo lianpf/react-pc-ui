ecb2690a0ecc8940c9a800b7ec2665fe2c72639c
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-06-21 20:44:56 +0800

    update@ template 组件增加 coding 展示

--- examples/template/index.js
+++ examples/template/index.js
@@ -4,6 +4,19 @@ import {connect} from 'react-redux';
 import './index.less';
 import Table from '../../src/component/table';
 
+function getChild(props, key) {
+  let childDom = null;
+  if (key + 1 > React.Children.count(props.children)) {
+    return childDom;
+  }
+  React.Children.forEach(props.children, (child, index) => {
+    if (index === key) {
+      childDom = React.cloneElement(child);
+    }
+  });
+  return childDom;
+}
+
 const columns = [{
   title: '参数',
   dataIndex: 'param',
@@ -25,9 +38,14 @@ const columns = [{
 class Index extends React.Component {
   constructor(props) {
     super(props);
+    // this.setState = {
+    //   demoDom: getChild(props, 0),
+    //   codingDom: getChild(props, 1),
+    // };
   }
 
   render() {
+    // let { codingDom, demoDom} = this.state;
     const { options } = this.props;
     return (
         <div className={`show-example-layout`}>
@@ -36,7 +54,10 @@ class Index extends React.Component {
           <h2>func</h2>
           <p>{options.func}</p>
           <h2>效果展示</h2>
-          {this.props.children}
+          {getChild(this.props, 0)}
+
+          <h2>代码展示</h2>
+          {getChild(this.props, 1)}
 
           <h2>API</h2>
           {
