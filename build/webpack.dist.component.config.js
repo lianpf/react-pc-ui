113c716abd93bba37bf6a5d27f2aef5289e9e514
连鹏飞 <pengfei.lian@yuntu-inc.com>
2018-06-25 09:58:54 +0800

    update@ 修改 webpack 配置

--- build/webpack.dist.component.config.js
+++ build/webpack.dist.component.config.js
@@ -17,20 +17,12 @@ module.exports = merge(webpackBaseConfig, {
     countdown: './src/component/countdown/index',
     pagination: './src/component/pagination/index',
   },
-  /**
-   * 多个入口起点
-   * 如果配置创建了多个单独的 "chunk"（例如，使用多个入口起点或使用像 CommonsChunkPlugin 这样的插件），则应该使用占位符(substitutions)来确保每个文件具有唯一的名称
-   * */
   output: {
     path: path.resolve(__dirname, '../lib'), //输出目标
     filename: '[name]/index.js',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  /**
-   * 剥离了那些不需要改动的依赖模块
-   * 防止将某些 import 的包(package)打包到 bundle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖(external dependencies)
-   * */
   externals: {
     'react': 'react',
     'react-dom': 'react-dom',
